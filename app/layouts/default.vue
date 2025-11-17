928776b6f1e0995a5277e4ea261379919b86815f
“Redskullvue” <pouya.9996@gmail.com>
2025-11-17 16:31:41 +0330

    init project

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full bg-blue-500 min-h-screen">
+    <header>Header will be here</header>
+    <slot />
+    <footer>Footer Will be here</footer>
+  </div>
+</template>
+
+<script setup></script>
