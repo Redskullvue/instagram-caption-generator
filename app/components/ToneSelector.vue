143d0f120796b1a6911305e5d69060818f49bc4e
“Redskullvue” <pouya.9996@gmail.com>
2025-11-19 18:34:46 +0330

    Feat : Tone Selector Created

---
+++ app/components/ToneSelector.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="w-full h-max">
+    <p>لحن خود را انتخاب کنید</p>
+    <div class="w-full min-h-5 grid grid-cols-2 mt-2">
+      <button
+        class="m-1 rounded-xl cursor-pointer py-2 transition-colors duration-300"
+        :class="
+          selectedTone === tone
+            ? 'bg-linear-to-r from-purple-600 to-pink-600 text-white'
+            : 'bg-white text-gray-500'
+        "
+        v-for="(tone, index) in tones"
+        :key="index"
+        @click="setTone(tone)"
+      >
+        {{ tone }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emits = defineEmits(["selectTone"]);
+const selectedTone = ref("رسمی");
+const tones = ref(["رسمی", "شوخ", "دوستانه", "عصبانی"]);
+
+const setTone = (value) => {
+  if (value) {
+    selectedTone.value = value;
+    emits("selectTone", selectedTone.value);
+    return;
+  }
+};
+</script>
---
+++ app/pages/chat/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full flex items-center justify-center h-full flex-col">
+    <!-- Chat box -->
+    <div class="w-full h-2/3">Chat Box</div>
+    <!-- Buttons and input Selector -->
+    <div
+      class="w-full h-1/3 border-t border-gray-500 p-2 overflow-y-scroll max-h-1/3 flex flex-col"
+    >
+      <ToneSelector class="w-full h-full" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "chat",
+});
+</script>
