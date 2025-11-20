d039f3734be14a625e5ce959f95fc94163fb1660
“Redskullvue” <pouya.9996@gmail.com>
2025-11-20 17:41:58 +0330

    Feat : LoginForm Created

---
+++ app/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-full h-full lg:p-4">
+    <form class="w-full h-full">
+      <label for="email" class="text-gray-800">آدرس ایمیل</label>
+      <div
+        class="w-full my-3 flex items-center justify-start gap-x-2 bg-gray-100 rounded-xl py-4 px-3"
+      >
+        <Icon
+          name="material-symbols:mail-outline"
+          size="25px"
+          class="text-gray-400"
+        />
+        <input
+          class="w-full focus:outline-0"
+          id="email"
+          type="email"
+          required
+          placeholder="example@gmail.com"
+        />
+      </div>
+      <label for="password" class="text-gray-800">رمز عبور</label>
+      <div
+        class="w-full my-3 flex items-center justify-start gap-x-2 bg-gray-100 rounded-xl py-4 px-3"
+      >
+        <Icon name="solar:lock-linear" size="23px" class="text-gray-400" />
+        <input
+          id="password"
+          class="w-full focus:outline-0"
+          type="password"
+          required
+          placeholder="رمز عبور(حداقل 8 کاراکتر)"
+        />
+      </div>
+      <div class="w-full mt-10">
+        <button
+          class="py-2 px-4 bg-linear-to-r from-purple-600 to-pink-600 rounded-xl text-white w-full cursor-pointer"
+        >
+          ورود به حساب
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
---
+++ app/pages/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full h-full flex items-center lg:p-10 p-4">
+    <div
+      class="w-full lg:w-10/12 h-full rounded-xl bg-white p-4 shadow-lg shadow-gray-300 flex flex-col"
+    >
+      <h2
+        class="w-full text-center text-lg font-semibold bg-linear-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent"
+      >
+        ورود به حساب کاربری
+      </h2>
+      <p class="w-full text-center text-gray-600 mt-3 mb-12">
+        به کپشن ساز خوش آمدید
+      </p>
+      <!-- Form -->
+      <div class="w-full h-full">
+        <LoginForm />
+      </div>
+      <div class="w-full text-center text-gray-600 text-sm">
+        فراموشی رمز عبور؟<span class="text-purple-500 mx-1"
+          ><NuxtLink to="/">دریافت رمز</NuxtLink></span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import LoginForm from "~/components/LoginForm.vue";
+
+definePageMeta({
+  layout: "auth",
+});
+</script>
