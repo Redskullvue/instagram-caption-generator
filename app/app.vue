2d3fd26118a66f8148cf74eecb3e2d71bf39f068
“Redskullvue” <pouya.9996@gmail.com>
2025-11-20 11:38:15 +0330

    Feat : New Scroll Style

--- app/app.vue
+++ app/app.vue
@@ -8,4 +8,23 @@
 html {
   font-family: Vazir, "sans-serif";
 }
+/* width */
+::-webkit-scrollbar {
+  width: 1px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
