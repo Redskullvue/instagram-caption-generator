408e6a4778c43f6d6d2ceb61967663a1b3ee7ae9
“Redskullvue” <pouya.9996@gmail.com>
2025-11-21 21:39:04 +0330

    Fix : Removing Server Side middleware

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,6 +2,9 @@ import { useAuthStore } from "@/stores/authStore";
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const auth = useAuthStore();
+  if (import.meta.server) {
+    return;
+  }
   const protectedRoutes = ["/chat"];
   if (protectedRoutes.includes(to.fullPath) && auth.isAuthenticated) {
     return;
