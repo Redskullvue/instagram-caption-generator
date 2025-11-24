7021004de720acfbe8249f62974630ba42e2cd13
“Redskullvue” <pouya.9996@gmail.com>
2025-11-24 16:48:32 +0330

    Feat : Sidebar BolierPlate created

---
+++ app/components/SideMobileMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-full flex flex-col h-[99vh] bg-white text-gray-800 p-4">
+    <div class="w-full flex items-center justify-start">
+      <button
+        @click="$emit('closeMenu', false)"
+        class="flex items-center gap lg:hidden"
+      >
+        بستن
+        <Icon
+          name="material-symbols-light:chevron-left"
+          size="20px"
+          class="font-bold"
+        />
+      </button>
+    </div>
+
+    This is the side menu
+  </div>
+</template>
+
+<script setup>
+const emits = defineEmits(["closeMenu"]);
+</script>
+
+<style lang="scss" scoped></style>
--- app/layouts/chat.vue
+++ app/layouts/chat.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="w-full h-screen flex justify-center items-center">
+  <div
+    class="w-full h-screen flex justify-center items-center overflow-x-hidden"
+  >
     <div
       class="w-full flex justify-start min-h-screen h-screen flex-col p-1 bg-linear-to-r from-purple-600 to-pink-600"
     >
@@ -14,6 +16,23 @@
           در چند ثانیه کپشن جذاب بسازید
         </p>
       </div>
+      <button
+        class="absolute left-4 top-10 lg:hidden text-white"
+        @click="showMenu"
+      >
+        <Icon name="stash:burger-classic" size="24px" />
+      </button>
+      <Transition name="slideInRight" appear>
+        <div
+          v-if="toggleMenu"
+          class="fixed inset-0 z-50 lg:z-0 overflow-hidden backdrop-blur-lg lg:backdrop-blur-none"
+        >
+          <div class="absolute left-0 top-0 h-full w-64 bg-white shadow-lg">
+            <SideMobileMenu @close-menu="closeMenu" />
+          </div>
+        </div>
+      </Transition>
+
       <div
         class="w-full lg:flex lg:justify-center h-full bg-linear-to-r from-purple-600 to-pink-600 p-1"
       >
@@ -27,4 +46,45 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const toggleMenu = ref(false);
+
+// Check if window is bigger than 1024px set toggleMenu to true
+
+onMounted(() => {
+  const width = window.innerWidth;
+  if (width >= 1024) {
+    toggleMenu.value = true;
+  }
+});
+
+const closeMenu = (val) => {
+  toggleMenu.value = val;
+};
+const showMenu = () => {
+  toggleMenu.value = true;
+};
+</script>
+<style>
+@keyframes SlideInRight {
+  from {
+    transform: translate3d(-25%, 0, 0);
+    visibility: visible;
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+/* When entering */
+.slideInRight-enter-active {
+  animation: SlideInRight 0.5s ease forwards;
+}
+
+/* When leaving */
+.slideInRight-leave-active {
+  animation: SlideInRight 0.5s ease reverse;
+}
+</style>
