8bb9001c9fa74b6c23eb4d38f2ca737597d47735
“Redskullvue” <pouya.9996@gmail.com>
2025-11-26 14:18:18 +0330

    Feat : new composable to change dates

---
+++ app/composables/useDateChanger.js
@@ -0,0 +1,34 @@
+export function useDateChanger(date) {
+  const formatDate = (date, options = {}) => {
+    const defaultOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      calendar: "persian",
+    };
+
+    const mergedOptions = { ...defaultOptions, ...options };
+    const newDate = new Date(date);
+    const faDate = newDate.toLocaleDateString("fa-IR", mergedOptions);
+
+    return faDate;
+  };
+
+  const timeAgo = (date) => {
+    const now = new Date();
+    const diffMs = now - new Date(date);
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+    if (diffDays === 0) return "امروز";
+    if (diffDays === 1) return "دیروز";
+    if (diffDays < 7) return `${diffDays} روز پیش`;
+    if (diffDays < 30) return `${Math.floor(diffDays / 7)} خفته پیش`;
+
+    return formatDate(date);
+  };
+
+  return {
+    formatDate,
+    timeAgo,
+  };
+}
