eb3330196eb6e796f16ca3a92409467963e4880b
“Redskullvue” <pouya.9996@gmail.com>
2025-11-29 19:03:24 +0330

    Fix : Missing await statement

--- server/api/chats/[id].delete.js
+++ server/api/chats/[id].delete.js
@@ -1,17 +1,19 @@
+// server/api/chats/[id].delete.js
 import { requireAuth } from "~~/server/utils/auth";
 import User from "~~/server/models/User";
 
 export default defineEventHandler(async (event) => {
-  const userId = requireAuth(event);
+  const userId = await requireAuth(event);
   const chatId = getRouterParam(event, "id");
-  const user = User.findById(userId);
+
+  const user = await User.findById(userId);
+
   if (!user) {
     throw createError({ statusCode: 404, message: "User not found" });
   }
 
   user.deleteChat(chatId);
   await user.save();
-  return {
-    success: true,
-  };
+
+  return { success: true };
 });
