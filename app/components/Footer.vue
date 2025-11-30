bf4aa7f485515ac5c3200ebf0f1875bcc63e3655
“Redskullvue” <pouya.9996@gmail.com>
2025-11-30 12:20:19 +0330

    Fix : Styling Issues

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="w-full flex items-center justify-between py-3 px-10 max-h-20 bg-white shadow-md shadow-gray-500"
+    class="w-full flex items-center justify-between py-3 lg:px-10 max-h-20 bg-white shadow-md shadow-gray-500 min-h-max"
   >
-    <div class="mx-4 flex items-center">
+    <div class="lg:mx-4 flex items-center w-full lg:w-max">
       <img
         class="max-w-[75px] max-h-[75px]"
         src="/logo.png"
@@ -11,7 +11,7 @@
       <h1 class="font-bold">کپشن ساز</h1>
     </div>
     <div>
-      <p class="text-sm text-gray-500">
+      <p class="lg:text-sm text-gray-500 w-full text-xs">
         © 2025 کپشن ساز. تمامی حقوق محفوظ است.
       </p>
     </div>
