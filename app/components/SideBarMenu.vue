a33d34eafec518467925a006a951a09c512e87cc
“Redskullvue” <pouya.9996@gmail.com>
2025-11-30 12:50:34 +0330

    Fix : Styling Issues

--- app/components/SideBarMenu.vue
+++ app/components/SideBarMenu.vue
@@ -16,17 +16,17 @@
     <div class="w-full bg-pink-100">
       <UserProfile />
     </div>
-    <div class="w-full mt-2 p-2">
+    <div class="w-full mt-2 p-2 border-b border-gray-300">
       <button
         @click="createNewChat"
-        class="rounded-xl bg-linear-to-r from-purple-600 to-pink-600 w-full text-white py-2 flex items-center gap-x-2 justify-center cursor-pointer transition-all duration-300 hover:opacity-80"
+        class="rounded-xl bg-linear-to-r from-purple-600 mb-2 to-pink-600 w-full text-white py-2 flex items-center gap-x-2 justify-center cursor-pointer transition-all duration-300 hover:opacity-80"
       >
         <Icon name="ic:outline-plus" size="24px" />
         ایجاد گفتگو جدید
       </button>
     </div>
     <div class="w-full h-full p-3">
-      <h2 class="mb-2 text-sm">تاریخچه گفت گو ها</h2>
+      <h2 class="mb-2 text-xs text-gray-500">تاریخچه گفت گو ها</h2>
       <ul
         class="w-full h-[68vh] rounded-xl p-4 overflow-y-auto pb-10"
         v-if="chatStore.chatHistory.length >= 1"
@@ -34,23 +34,33 @@
         <li
           v-for="(chat, index) in chatStore.chatHistory"
           :key="index"
-          class="rounded-xl p-2 w-full mb-3 cursor-pointer shadow-md shadow-gray-400"
+          class="rounded-xl p-2 w-full mb-3 cursor-pointer border border-gray-300 flex"
           :class="
-            chatStore.currentChatId === chat.id ? 'bg-pink-100' : 'bg-gray-100'
+            chatStore.currentChatId === chat.id ? 'bg-purple-100' : 'bg-gray-50'
           "
         >
-          <div @click="handleLoadChat(chat.id)">
-            <h2 class="mb-3">{{ chat.title }}</h2>
-            <p class="text-gray-700 text-sm mb-4">
-              {{ chat.lastMessage.substring(0, 25) }}....
-            </p>
+          <div class="w-max mx-1 p-1 mt-1">
+            <Icon name="tdesign:chat" size="15px" class="text-purple-500" />
           </div>
+          <div @click="handleLoadChat(chat.id)" class="w-full p-2">
+            <div>
+              <h2 class="mb-3 text-gray-500 text-sm font-light">
+                {{ chat.title.substring(0, 20) }}...
+              </h2>
+              <p
+                class="text-gray-500 text-[10px] mt-1 font-light mb-4"
+                v-if="chat.lastMessage"
+              >
+                {{ chat.lastMessage.substring(0, 25) }}....
+              </p>
+            </div>
 
-          <div class="w-full flex items-center justify-between">
-            <p class="text-xs">{{ timeAgo(chat.createdAt) }}</p>
-            <p @click="deleteChat(chat.id)" class="text-red-500">
-              <Icon name="line-md:trash" size="20px" />
-            </p>
+            <div class="w-full flex items-center justify-between">
+              <p class="text-xs text-gray-400">{{ timeAgo(chat.createdAt) }}</p>
+              <p @click="deleteChat(chat.id)" class="text-red-300">
+                <Icon name="line-md:trash" size="20px" />
+              </p>
+            </div>
           </div>
         </li>
       </ul>
