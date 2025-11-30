c3b9e4dd2ddf0f5ed0db419e7ceee98fa3dd770d
“Redskullvue” <pouya.9996@gmail.com>
2025-11-30 14:20:18 +0330

    Fix : Styling Issues

--- app/components/ToneSelector.vue
+++ app/components/ToneSelector.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full h-max">
     <p>لحن خود را انتخاب کنید</p>
-    <div class="w-full min-h-5 grid grid-cols-2 mt-2">
+    <div class="w-full min-h-5 grid grid-cols-2 mt-2 text-sm">
       <button
-        class="m-1 rounded-xl cursor-pointer py-2 transition-colors duration-300"
+        class="m-1 rounded-xl cursor-pointer py-3 transition-colors duration-300 shadow-sm shadow-gray-300"
         :class="
           selectedTone === tone.value
             ? 'bg-linear-to-r from-purple-600 to-pink-600 text-white'
-            : 'bg-gray-200 text-gray-700'
+            : 'bg-gray-100 text-gray-700'
         "
         v-for="(tone, index) in tones"
         :key="index"
