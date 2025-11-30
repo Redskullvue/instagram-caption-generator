09a6584e3e79a3d97a907623a2fb8b39451c5348
“Redskullvue” <pouya.9996@gmail.com>
2025-11-30 12:58:16 +0330

    Fix : Typo

--- app/components/ChatBubble.vue
+++ app/components/ChatBubble.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-max max-w-[90%] whitespace-pre-lin lg:min-h-[70px] mt-4 p-3 rounded-xl min-h-[10] shadow-md shadow-gray-500 flex items-center justify-center"
+    class="w-max max-w-[90%] whitespace-pre-line lg:min-h-[70px] mt-4 p-3 rounded-xl min-h-[10] shadow-md shadow-gray-500 flex items-center justify-center"
     :class="
       message.isUser
         ? 'ml-auto bg-linear-to-r  from-purple-600 to-pink-600 text-white'
