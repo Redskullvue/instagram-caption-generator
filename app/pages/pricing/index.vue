aa17d63bbc7b8cd521cea49bad45fdbf0fb4a9d9
“Redskullvue” <pouya.9996@gmail.com>
2025-12-01 14:10:30 +0330

    Feat : New Pricing Page

---
+++ app/pages/pricing/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="min-h-screen w-full bg-pink-100 flex flex-col items-center p-10">
+    <Transition name="fade" appear>
+      <div class="bg-white py-2 rounded-xl px-8 mb-20">
+        <p
+          class="bg-linear-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent"
+        >
+          پلن ها
+        </p>
+      </div>
+    </Transition>
+    <div class="w-full flex items-center justify-center p-1 mb-4">
+      <p class="text-center w-full text-3xl lg:text-4xl">
+        پلنی که برای
+        <span
+          class="bg-linear-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent"
+          >شما</span
+        >
+        مناسب هست رو انتخاب کنید
+      </p>
+    </div>
+    <p
+      class="text-center w-full text-balance text-gray-600 mb-10 lg:max-w-[80ch]"
+    >
+      از رایگان شروع کنید و با رشد خود، پلن را ارتقا دهید. بدون تعهد، لغو هر
+      زمان که بخواهید
+    </p>
+    <!-- Plans -->
+    <div
+      class="w-full grid grid-cols-1 place-items-center gap-x-8 gap-y-6 lg:grid-cols-3 mt-10"
+    >
+      <div
+        v-for="(plan, index) in plans"
+        :key="index"
+        class="bg-white shadow-md shadow-gray-300 p-10 w-full min-h-[680px] rounded-xl flex items-center flex-col"
+      >
+        <div class="w-full flex items-center justify-center">
+          <img
+            :src="plan.image"
+            alt="Plans image"
+            class="w-[110px] h-[110px] object-contain"
+          />
+        </div>
+        <h2 class="w-full text-center text-2xl mt-3 lg:mt-1 lg:text-3xl">
+          {{ plan.title }}
+        </h2>
+        <p class="w-full text-gray-500 text-center mt-4 text-sm">
+          {{ plan.description }}
+        </p>
+        <p
+          class="text-sm text-red-400 line-through mt-10"
+          v-if="plan.mainPrice"
+        >
+          {{ plan.mainPrice }}تومان
+        </p>
+        <h3 class="mt-6 text-2xl lg:text-3xl">{{ plan.price || "رایگان" }}</h3>
+        <button
+          class="w-full mt-10 bg-red-500 text-white rounded-xl py-3 cursor-pointer transition-all duration-300 hover:opacity-80"
+          :class="
+            plan.title === 'Free'
+              ? 'bg-slate-800'
+              : 'bg-linear-to-r from-purple-600 to-pink-600'
+          "
+        >
+          شروع {{ plan.title }}
+        </button>
+        <div class="w-full text-start mt-10">
+          <h4>امکانات :</h4>
+        </div>
+        <ul class="flex flex-col gap-y-3 w-full mt-3">
+          <li
+            v-for="(product, index) in plan.products"
+            :key="index"
+            class="w-full flex items-center gap-x-3"
+            :class="product.isValid ? 'text-gray-700' : 'text-gray-400'"
+          >
+            <Icon
+              name="lets-icons:check-fill"
+              size="23px"
+              v-if="product.isValid"
+            />
+            <Icon
+              name="zondicons:close-solid"
+              size="20px"
+              v-if="!product.isValid"
+            />
+            {{ product.title }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const plans = [
+  {
+    title: "Free",
+    price: "رایگان",
+    image: "/Pricing-1.png",
+    description: "برای شروع و آزمایش",
+    products: [
+      { title: "5 کپشن در ماه", isValid: true },
+      { title: "دسترسی به هشتگ ها", isValid: true },
+      { title: "پشتیبانی اولویت دار", isValid: false },
+      { title: "دسترسی به سناریو نویس", isValid: false },
+    ],
+  },
+  {
+    title: "Pro",
+    price: "۴۹.۹۰۰ تومان",
+    mainPrice: "۱۱۹,۹۰۰",
+    image: "/Pricing-2.png",
+    description: "برای سازندگان محتوای حرفه‌ای",
+    products: [
+      { title: "70 کپشن در ماه", isValid: true },
+      { title: "دسترسی به هشتگ ها", isValid: true },
+      { title: "پشتیبانی اولویت دار", isValid: true },
+      { title: "دسترسی به سناریو نویس", isValid: true },
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "۷۹.۹۰۰ تومان ",
+    mainPrice: "۲۱۹,۹۰۰",
+    image: "/Pricing-3.png",
+    description: "برای کسب‌وکارها و آژانس‌ها",
+    products: [
+      { title: "کپشن های نامحدود", isValid: true },
+      { title: "دسترسی به هشتگ ها", isValid: true },
+      { title: "پشتیبانی اولویت دار", isValid: true },
+      { title: "دسترسی به سناریو نویس", isValid: true },
+    ],
+  },
+];
+</script>
