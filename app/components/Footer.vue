c2e196134dcec8b6a3ab69b0af5a966638357877
“Redskullvue” <pouya.9996@gmail.com>
2025-12-02 12:21:34 +0330

    Feat : New links added to footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,19 +1,25 @@
 <template>
   <div
-    class="w-full flex items-center justify-between py-3 lg:px-10 max-h-20 bg-white shadow-md shadow-gray-500 min-h-max"
+    class="w-full flex flex-col justify-between py-3 lg:px-10 max-h-20 bg-white shadow-md shadow-gray-500 min-h-max"
   >
-    <div class="lg:mx-4 flex items-center w-full lg:w-max">
-      <img
-        class="max-w-[75px] max-h-[75px]"
-        src="/logo.png"
-        alt="لوگو-کپشن ساز"
-      />
-      <h1 class="font-bold">کپشن ساز</h1>
+    <div class="flex w-full items-center justify-between">
+      <div class="lg:mx-4 flex items-center w-full lg:w-max">
+        <img
+          class="max-w-[75px] max-h-[75px]"
+          src="/logo.png"
+          alt="لوگو-کپشن ساز"
+        />
+        <h1 class="font-bold">کپشن ساز</h1>
+      </div>
+      <div>
+        <p class="lg:text-sm text-gray-500 w-full text-xs">
+          © 2025 کپشن ساز. تمامی حقوق محفوظ است.
+        </p>
+      </div>
     </div>
-    <div>
-      <p class="lg:text-sm text-gray-500 w-full text-xs">
-        © 2025 کپشن ساز. تمامی حقوق محفوظ است.
-      </p>
+    <div class="w-full px-10 mt-4 flex gap-x-7">
+      <NuxtLink to="/about">درباره ما</NuxtLink>
+      <NuxtLink to="/pricing">قیمت ها</NuxtLink>
     </div>
   </div>
 </template>
