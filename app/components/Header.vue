da089df187dbaf5e5cf5253580cdc4cdb76a8348
“Redskullvue” <pouya.9996@gmail.com>
2025-12-06 14:13:04 +0330

    Feat : Fallback link to / page

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -3,12 +3,14 @@
     class="w-full flex items-center justify-between p-4 max-h-20 bg-white shadow-md shadow-gray-500"
   >
     <div class="mx-4 flex items-center">
-      <img
-        class="max-w-[75px] max-h-[75px]"
-        src="/logo.png"
-        alt="لوگو-کپشن ساز"
-      />
-      <h1 class="font-bold">کپشن ساز</h1>
+      <NuxtLink class="flex items-center" to="/">
+        <img
+          class="max-w-[75px] max-h-[75px]"
+          src="/logo.png"
+          alt="لوگو-کپشن ساز"
+        />
+        <h1 class="font-bold">کپشن ساز</h1>
+      </NuxtLink>
     </div>
     <div>
       <NuxtLink
