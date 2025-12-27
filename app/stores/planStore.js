96a8c0e7f1cd23d0acb2ff33f7261a8f65872c34
“Redskullvue” <pouya.9996@gmail.com>
2025-12-27 22:29:05 +0330

    Fix : Status Will update in realtime front-end

--- app/stores/planStore.js
+++ app/stores/planStore.js
@@ -114,12 +114,12 @@ export const usePlanStore = defineStore("planstore", () => {
           completed: status,
         },
       });
+      await hydrate();
+      toastStore.addToast("success", "به‌روزرسانی شد");
       // Update the realtime schedule
       let index = currentPlan.value.schedule.findIndex(
         (item) => item.id === scheduleId
       );
-      await hydrate();
-      toastStore.addToast("success", "به‌روزرسانی شد");
       currentPlan.value.schedule[index].completed = status;
     } catch (error) {
       throw new Error("خطا در ارتباط با سرور");
