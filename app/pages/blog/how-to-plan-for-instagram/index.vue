f301d84e765d1ae4ea25fe001c2e47492e0de45f
“Redskullvue” <pouya.9996@gmail.com>
2025-12-28 12:36:16 +0330

    Fix : RTL-LTR config Issue

---
+++ app/pages/blog/how-to-plan-for-instagram/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full flex items-center justify-center bg-pink-100 lg:p-10 p-4">
+    <div
+      class="w-full min-h-screen 2xl:w-[80%] p-3 flex items-center flex-col bg-white rounded-xl shadow-md shadow-gray-300"
+    >
+      <!-- Header -->
+      <div class="w-full flex-col gap-y-3 mb-4 p-3">
+        <h1 class="text-2xl font-bold">
+          چطور هوش مصنوعی نوشتن کپشن‌های اینستاگرام را متحول کرده است؟ معرفی وب
+          اپلیکیشن ما و پشت‌صحنه عملکرد آن
+        </h1>
+      </div>
+      <!-- Blog Image -->
+      <div class="p-2 mb-10">
+        <NuxtImg
+          src="/blog-1.png"
+          alt="error.preview"
+          class="object-contain rounded-xl"
+        />
+      </div>
+
+      <!-- POST COntent -->
+      <p class="leading-10 text-gray-700">
+        در سال‌های اخیر تولید محتوا در شبکه‌های اجتماعی، مخصوصاً اینستاگرام، به
+        یک مهارت تخصصی تبدیل شده است. کاربران برای جلب توجه، افزایش تعامل و
+      </p>
+      <h2 class="mt-4 w-full text-start text-xl font-bold">نحوه استفاده :</h2>
+
+      <p class="text-gray-700 leading-10 mt-4">
+        اولین قدم انتخاب پلتفرم است. شما مشخص می‌کنید کپشن برای اینستاگرام،
+        تیک‌تاک، توییتر/X یا لینکدین نوشته شود. چون هر پلتفرم سبک و لحن مخصوص
+        خودش را دارد، اپلیکیشن به صورت خودکار ساختار مناسب همان شبکه را انتخاب
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
