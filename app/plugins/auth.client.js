3789d17aeba300d76c52ac1d409b1d3705335315
“Redskullvue” <pouya.9996@gmail.com>
2025-12-28 13:16:38 +0330

    Fix : Computed Properties not working on hydration

--- app/plugins/auth.client.js
+++ app/plugins/auth.client.js
@@ -2,11 +2,16 @@ export default defineNuxtPlugin(async () => {
   const authStore = useAuthStore();
   const usageStore = useUsageStore();
   const chatStore = useChatStore();
+  const planStore = usePlanStore();
   // Hydrate auth first
   authStore.hydrate();
 
   // Then hydrate usage if authenticated
   if (authStore.token) {
-    await Promise.all([usageStore.hydrate(), chatStore.hydrate(), ,]);
+    await Promise.all([
+      usageStore.hydrate(),
+      chatStore.hydrate(),
+      planStore.hydrate(),
+    ]);
   }
 });
