3df9c39f08a0326a35afdd1ff0c2558725b5b57e
“Redskullvue” <pouya.9996@gmail.com>
2026-01-05 19:48:18 +0330

    Fix : Better Styling For Tags

--- app/components/Tag.vue
+++ app/components/Tag.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="rounded-xl px-2 py-1"
+    class="rounded-xl px-3 py-1 border"
     :class="{
-      'text-blue-600 bg-blue-300': color === 'blue',
-      'text-green-600 bg-green-300': color === 'green',
-      'text-red-600 bg-red-300': color === 'red',
-      'text-yellow-600 bg-yellow-300': color === 'yellow',
+      'text-blue-600 bg-blue-300 border-blue-600': color === 'blue',
+      'text-green-600 bg-green-300 border-green-600': color === 'green',
+      'text-red-600 bg-red-300 border-red-600': color === 'red',
+      'text-yellow-600 bg-yellow-300 border-yellow-600': color === 'yellow',
     }"
   >
     {{ text }}
