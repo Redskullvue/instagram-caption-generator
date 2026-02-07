22e1fedbd515ea861b5b7c78c026bdd8e2a33972
“Redskullvue” <pouya.9996@gmail.com>
2026-02-07 11:57:29 +0330

    Feat : New Endpoint to recive data for a single Ticket

---
+++ server/api/tickets/[id]/index.get.js
@@ -0,0 +1,27 @@
+import Ticket from "~~/server/models/Tickets";
+import User from "~~/server/models/User";
+import { requireAuth } from "~~/server/utils/auth";
+export default defineEventHandler(async (event) => {
+  const ticketId = getRouterParam(event, "id");
+  try {
+    const userId = await requireAuth(event);
+    const user = await User.findById(userId);
+    if (!user) {
+      throw createError({
+        status: 404,
+        message: "کاربر وجود ندارد",
+      });
+    }
+    const ticket = await Ticket.findById(ticketId);
+
+    return {
+      success: true,
+      ticket: ticket,
+    };
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      message: "مشکل در دسترسی به سرور ",
+    });
+  }
+});
